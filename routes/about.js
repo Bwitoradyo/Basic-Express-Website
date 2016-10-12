92597a46ae5f9818c5b375e179d4ff9abd6d6645
Bambang Witoradyo <bambang.witoradyo@sonoco.com>
2016-10-11 22:49:08 -0400

    add routing

--- routes/about.js
+++ routes/about.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//non need for  '/about' because it is its own file
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+    res.render('about', { title: 'About' });
 });
 
 module.exports = router;
